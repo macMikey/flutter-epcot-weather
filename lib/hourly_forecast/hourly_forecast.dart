ef29f24768728f68ba4a3dc1497d7ecb9a3ed59b
mikey <mike@mikekerner.com>
2024-08-24 15:25:23 -0400

    add all weather icons

--- lib/hourly_forecast/hourly_forecast.dart
+++ lib/hourly_forecast/hourly_forecast.dart
@@ -12,14 +12,17 @@ class HourlyForecast extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
-          HourlyForecastItem(hour: "08:00", icon: Icons.cloud, temperature: "80 °F"),
+          HourlyForecastItem(hour: "08:00", icon: Icons.wb_sunny, temperature: "80 °F"),
           HourlyForecastItem(hour: "09:00", icon: Icons.cloud, temperature: "80 °F"),
-          HourlyForecastItem(hour: "10:00", icon: Icons.cloud, temperature: "80 °F"),
-          HourlyForecastItem(hour: "11:00", icon: Icons.cloud, temperature: "80 °F"),
-          HourlyForecastItem(hour: "12:00", icon: Icons.cloud, temperature: "80 °F"),
-          HourlyForecastItem(hour: "13:00", icon: Icons.cloud, temperature: "80 °F"),
-          HourlyForecastItem(hour: "14:00", icon: Icons.cloud, temperature: "80 °F"),
-          HourlyForecastItem(hour: "15:00", icon: Icons.cloud, temperature: "80 °F"),
+          HourlyForecastItem(hour: "10:00", icon: Icons.grain, temperature: "80 °F"), // looks like blizzard
+          HourlyForecastItem(hour: "11:00", icon: Icons.flash_on, temperature: "80 °F"),
+          HourlyForecastItem(hour: "12:00", icon: Icons.ac_unit, temperature: "80 °F"), //snowflake
+          HourlyForecastItem(hour: "13:00", icon: Icons.air, temperature: "80 °F"), // wind
+          HourlyForecastItem(hour: "14:00", icon: Icons.blur_on, temperature: "80 °F"), // no idea. they say foggy
+          HourlyForecastItem(hour: "15:00", icon: Icons.nights_stay, temperature: "80 °F"),
+          HourlyForecastItem(
+              hour: "15:00", icon: Icons.wb_cloudy, temperature: "80 °F"), // cloudy (they say "partly cloudy")
+          HourlyForecastItem(hour: "15:00", icon: Icons.thermostat, temperature: "80 °F"),
         ],
       ),
     );
