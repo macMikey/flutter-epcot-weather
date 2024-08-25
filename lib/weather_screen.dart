782a74a243a1f3d25d9c459e1aab294bfdb3d362
mikey <mike@mikekerner.com>
2024-08-24 21:41:48 -0400

    http 1

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -2,9 +2,30 @@
 import 'package:weather/app_body.dart';
 import 'package:weather/app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather/secrets.dart';
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getCurrentWeather();
+  }
+
+  Future getCurrentWeather() async {
+    var lat = 28.376824;
+    var lon = -81.549395;
+    final res = await http.get(Uri.parse(
+        "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&current_weather=true&daily=weather_code,temperature_2m_max,temperature_2m_min,uv_index_max,rain_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York"));
+    debugPrint(res.body);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,5 +36,5 @@ class WeatherScreen extends StatelessWidget {
       ),
       body: AppBody(),
     );
-  } // build
+  }
 }
