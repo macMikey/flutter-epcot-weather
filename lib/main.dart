32f02bb03f202d789d266d52655ac2c892219e43
mikey <mike@mikekerner.com>
2024-08-27 15:13:09 -0400

    fix mode issues

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,25 @@ void main() {
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeNotifier(),
-      child: MyApp(),
+      child: const MyApp(),
     ),
   );
 }
 // --------------------------------------------------------------
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Converted 'key' to a super parameter
+
   @override
   Widget build(BuildContext context) {
     return Consumer<ThemeNotifier>(
       builder: (context, themeNotifier, child) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false, // Removed the debug flag
           themeMode: themeNotifier.themeMode,
           theme: ThemeData.light(),
           darkTheme: ThemeData.dark(),
-          home: WeatherScreen(),
+          home: const WeatherScreen(), // Added 'const' to the constructor
         );
       },
     );
